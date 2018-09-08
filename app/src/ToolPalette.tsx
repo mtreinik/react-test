390df0dc6ed7175e722178a0276748024a43206f
Mikko Reinikainen <mikko.reinikainen@iki.fi>
2018-09-08 09:35:41 +0300

    added statefulness to pixel

--- app/src/ToolPalette.tsx
+++ app/src/ToolPalette.tsx
@@ -5,13 +5,13 @@ const COLORS: string[] = [
   '#000000', '#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff', '#ffffff'
 ];
 
-export default class ColorPalette extends React.Component<{}> {
+export default class ToolPalette extends React.Component<{}> {
   constructor(props:any) {
     super(props);
   }
   render() {
     return (
-      <div className="color-palette">
+      <div className="tool-palette">
         { COLORS.map((color) => <ColorSelector color={ color } />) }
       </div>
     );
