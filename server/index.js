41a9a8e1f811ccccc5136bef94dc80f71e87543b
Mikko Reinikainen <mikko.reinikainen@iki.fi>
2018-09-10 00:39:52 +0300

    Revert "use TypeScript for server"
    This reverts commit e16ce4957e524765fc6bc6721f786a66e50a01fc.

--- server/index.js
+++ server/index.js
@@ -1,26 +1,25 @@
 const path = require('path');
 const express = require('express');
 const http = require('http');
-const WS = require('ws');
-import { Request, Response } from 'express';
+const WebSocket = require('ws');
 
 const port = process.env.PORT || 80;
 
 // A HTTP server with routing through express
 const app = express();
 app.use(express.static(path.resolve(__dirname, '../react-ui/app')));
-app.get('*', (request:Request, response:Response) => {
+app.get('*', (request, response) => {
     response.sendFile(path.resolve(__dirname, '../react-ui/app', 'index.html'));
 });
 const httpServer = http.createServer(app);
 
 // A WebSocket server on top of the HTTP server
-const wss = new WS.Server({
+const wss = new WebSocket.Server({
     'server': httpServer
 });
-wss.on('connection', (ws:any) => {
-  ws.on('message', (data:string) => {
-    wss.clients.forEach((client:any) => {
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    wss.clients.forEach((client) => {
       console.log('ws broadcasting' + JSON.stringify(data));
       client.send(data);
     });
