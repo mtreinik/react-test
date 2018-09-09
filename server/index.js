8bd3e3d4b62433283e603f0a41eb0562e2b9e758
Mikko Reinikainen <mikko.reinikainen@iki.fi>
2018-09-09 22:57:07 +0300

    added HTTP server for react-UI and WebSocket

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express')
+const http = require('http')
+const WebSocket = require('ws')
+const port = process.env.PORT || 8080
 
-const WebSocket = require('ws');
-const WEBSOCKET_OPTIONS = { port: PORT };
-console.log('starting WebSockets Server with options: ' + JSON.stringify(WEBSOCKET_OPTIONS));
-const wss = new WebSocket.Server(WEBSOCKET_OPTIONS);
+const app = express()
+app.use(express.static(path.resolve(__dirname, '../react-ui/app')));
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/app', 'index.html'));
+});
 
-// Broadcast to all.
+const httpServer = http.createServer(app)
+
+const wss = new WebSocket.Server({
+    'server': httpServer
+})
+
+//TODO is this needed?
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
@@ -27,3 +36,5 @@ wss.on('connection', function connection(ws) {
     });
   });
 });
+
+httpServer.listen(port)
