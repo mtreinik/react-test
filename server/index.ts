e16ce4957e524765fc6bc6721f786a66e50a01fc
Mikko Reinikainen <mikko.reinikainen@iki.fi>
2018-09-09 23:59:04 +0300

    use TypeScript for server

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,26 @@
 const path = require('path');
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
+const WS = require('ws');
+import { Request, Response } from 'express';
 
 const port = process.env.PORT || 80;
 
 // A HTTP server with routing through express
 const app = express();
 app.use(express.static(path.resolve(__dirname, '../react-ui/app')));
-app.get('*', (request, response) => {
+app.get('*', (request:Request, response:Response) => {
     response.sendFile(path.resolve(__dirname, '../react-ui/app', 'index.html'));
 });
 const httpServer = http.createServer(app);
 
 // A WebSocket server on top of the HTTP server
-const wss = new WebSocket.Server({
+const wss = new WS.Server({
     'server': httpServer
 });
-wss.on('connection', (ws) => {
-  ws.on('message', (data) => {
-    wss.clients.forEach((client) => {
+wss.on('connection', (ws:any) => {
+  ws.on('message', (data:string) => {
+    wss.clients.forEach((client:any) => {
       console.log('ws broadcasting' + JSON.stringify(data));
       client.send(data);
     });
