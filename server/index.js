e13628b53faf664b362a9a555bc4fe9fdbf56905
Mikko Reinikainen <mikko.reinikainen@iki.fi>
2018-09-09 23:05:51 +0300

    try without websocket broadcast

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const wss = new WebSocket.Server({
 })
 
 //TODO is this needed?
+/*
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
@@ -25,6 +26,7 @@ wss.broadcast = function broadcast(data) {
     }
   });
 };
+*/
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
